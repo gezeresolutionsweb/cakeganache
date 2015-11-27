9564f1380973ccc7936061e1adc924981955974a
Sylvain Lévesque <slevesque@gezere.com>
2015-11-27 14:07:18 -0500

    * Add Test base structure.

---
+++ Test/Case/AllTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class AllTest extends CakeTestSuite {
+    public static function suite() {
+        $suite = new CakeTestSuite('All CakeGanache tests');
+        $suite->addTestDirectoryRecursive(__DIR__ . DS . 'View');
+        return $suite;
+    }
+}
---
+++ Test/Case/View/Helper/GanacheHtmlHelperTest.php
@@ -0,0 +1,8 @@
+<?php
+class GanacheHtmlHelperTest extends CakeTestCase
+{
+    public function testLink()
+    {
+        $this->assertEquals(0, 0);
+    }
+}
