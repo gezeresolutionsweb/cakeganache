882175c8dd8c0f03d7005b6129d6755016e4dfbe
Sylvain Lévesque <slevesque@gezere.com>
2015-12-02 13:17:14 -0500

    * Complete GanacheHtmlHelper::icon() tests.

--- Test/Case/View/Helper/GanacheHtmlHelperTest.php
+++ Test/Case/View/Helper/GanacheHtmlHelperTest.php
@@ -1,8 +1,106 @@
 <?php
+App::uses('Controller', 'Controller');
+App::uses('View', 'View');
+App::uses('GanacheHtmlHelper', 'CakeGanache.View/Helper');
+
+
 class GanacheHtmlHelperTest extends CakeTestCase
 {
-    public function testLink()
+    public function setUp()
     {
-        $this->assertEquals(0, 0);
+        parent::setUp();
+        $Controller = new Controller();
+        $View = new View($Controller);
+        $this->GanacheHtmlHelper = new GanacheHtmlHelper($View);
+    }
+
+    public function testSimpleIconWithCustomPrefix()
+    {
+        $Controller = new Controller();
+        $View = new View($Controller);
+        $ganacheHtmlHelper = new GanacheHtmlHelper($View, ['prefix' => 'fa']);
+        $result = $ganacheHtmlHelper->icon('home');
+        $expected = '<i class="fa fa-home"></i>';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testSimpleIconSizeLgWithCustomPrefix()
+    {
+        $Controller = new Controller();
+        $View = new View($Controller);
+        $ganacheHtmlHelper = new GanacheHtmlHelper($View, ['prefix' => 'fa']);
+        $result = $ganacheHtmlHelper->icon('home', ['ga_size' => GA_LG]);
+        $expected = '<i class="fa fa-home fa-lg"></i>';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testSimpleIcon()
+    {
+        $result = $this->GanacheHtmlHelper->icon('home');
+        $expected = '<i class="glyphicon glyphicon-home"></i>';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testIconSizeLg()
+    {
+        $result = $this->GanacheHtmlHelper->icon('home', ['ga_size' => GA_LG]);
+        $expected = '<i class="glyphicon glyphicon-home glyphicon-lg"></i>';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testIconSize1x()
+    {
+        $result = $this->GanacheHtmlHelper->icon('home', ['ga_size' => GA_1X]);
+        $expected = '<i class="glyphicon glyphicon-home glyphicon-1x"></i>';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testIconSize2x()
+    {
+        $result = $this->GanacheHtmlHelper->icon('home', ['ga_size' => GA_2X]);
+        $expected = '<i class="glyphicon glyphicon-home glyphicon-2x"></i>';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testIconSize3x()
+    {
+        $result = $this->GanacheHtmlHelper->icon('home', ['ga_size' => GA_3X]);
+        $expected = '<i class="glyphicon glyphicon-home glyphicon-3x"></i>';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testIconSize4x()
+    {
+        $result = $this->GanacheHtmlHelper->icon('home', ['ga_size' => GA_4X]);
+        $expected = '<i class="glyphicon glyphicon-home glyphicon-4x"></i>';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testIconSize5x()
+    {
+        $result = $this->GanacheHtmlHelper->icon('home', ['ga_size' => GA_5X]);
+        $expected = '<i class="glyphicon glyphicon-home glyphicon-5x"></i>';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testIconSpin()
+    {
+        $result = $this->GanacheHtmlHelper->icon('spinner', ['ga_spin' => true]);
+        $expected = '<i class="glyphicon glyphicon-spinner glyphicon-spin"></i>';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testIconWithClassAttributeAsString()
+    {
+        $result = $this->GanacheHtmlHelper->icon('home', ['class' => 'spinorama']);
+        $expected = '<i class="glyphicon glyphicon-home spinorama"></i>';
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testIconWithClassAttributeAsArray()
+    {
+        $result = $this->GanacheHtmlHelper->icon('home', ['class' => ['spinorama', 'spinoranissima']]);
+        $expected = '<i class="glyphicon glyphicon-home spinorama spinoranissima"></i>';
+        $this->assertEquals($expected, $result);
     }
 }
