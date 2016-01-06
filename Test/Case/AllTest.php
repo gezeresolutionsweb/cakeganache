bee30a4b769843463b89184fd35f7750b711a258
Sylvain Lévesque <slevesque@gezere.com>
2016-01-06 11:46:26 -0500

    * Add Config directory to AllTest cases.

--- Test/Case/AllTest.php
+++ Test/Case/AllTest.php
@@ -3,6 +3,7 @@
 class AllTest extends CakeTestSuite {
     public static function suite() {
         $suite = new CakeTestSuite('All CakeGanache tests');
+        $suite->addTestDirectoryRecursive(__DIR__ . DS . 'Config');
         $suite->addTestDirectoryRecursive(__DIR__ . DS . 'View');
         return $suite;
     }
