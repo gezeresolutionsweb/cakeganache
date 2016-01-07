c7e2d04e8d5142758ecb311c1d798374da99bdef
Sylvain Lévesque <slevesque@gezere.com>
2016-01-07 10:49:55 -0500

    * Add new bootstrap3.php file that is use to store temporarly bootstrap 3 constants. Not present in bootstrap 2, so we don't want to use it. But we want to use it later when we will migrate the library to bootstrap 3.

---
+++ Config/bootstrap3.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * CakeGanache : (https://github.com/gezeresolutionsweb/cakeganache)
+ * Copyright (c) Sylvain Lévesque (http://www.gezere.com)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Sylvain Lévesque (http://www.gezere.com)
+ * @link          http://www.gezere.com
+ * @package       CakeGanache.View.Helper
+ * @since         CakeGanache v 0.0.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+// This files contain bootstrap 3 classes for later use.
+define('GA_XS1', 'col-xs-1'); // BS3 only @test
+define('GA_XS2', 'col-xs-2'); // BS3 only @test
+define('GA_XS3', 'col-xs-3'); // BS3 only @test
+define('GA_XS4', 'col-xs-4'); // BS3 only @test
+define('GA_XS5', 'col-xs-5'); // BS3 only @test
+define('GA_XS6', 'col-xs-6'); // BS3 only @test
+define('GA_XS7', 'col-xs-7'); // BS3 only @test
+define('GA_XS8', 'col-xs-8'); // BS3 only @test
+define('GA_XS9', 'col-xs-9'); // BS3 only @test
+define('GA_XS10', 'col-xs-10'); // BS3 only @test
+define('GA_XS11', 'col-xs-11'); // BS3 only @test
+define('GA_XS12', 'col-xs-12'); // BS3 only @test
+define('GA_SM1', 'col-sm-1'); // BS3 only @test
+define('GA_SM2', 'col-sm-2'); // BS3 only @test
+define('GA_SM3', 'col-sm-3'); // BS3 only @test
+define('GA_SM4', 'col-sm-4'); // BS3 only @test
+define('GA_SM5', 'col-sm-5'); // BS3 only @test
+define('GA_SM6', 'col-sm-6'); // BS3 only @test
+define('GA_SM7', 'col-sm-7'); // BS3 only @test
+define('GA_SM8', 'col-sm-8'); // BS3 only @test
+define('GA_SM9', 'col-sm-9'); // BS3 only @test
+define('GA_SM10', 'col-sm-10'); // BS3 only @test
+define('GA_SM11', 'col-sm-11'); // BS3 only @test
+define('GA_SM12', 'col-sm-12'); // BS3 only @test
+define('GA_MD1', 'col-md-1'); // BS3 only @test
+define('GA_MD2', 'col-md-2'); // BS3 only @test
+define('GA_MD3', 'col-md-3'); // BS3 only @test
+define('GA_MD4', 'col-md-4'); // BS3 only @test
+define('GA_MD5', 'col-md-5'); // BS3 only @test
+define('GA_MD6', 'col-md-6'); // BS3 only @test
+define('GA_MD7', 'col-md-7'); // BS3 only @test
+define('GA_MD8', 'col-md-8'); // BS3 only @test
+define('GA_MD9', 'col-md-9'); // BS3 only @test
+define('GA_MD10', 'col-md-10'); // BS3 only @test
+define('GA_MD11', 'col-md-11'); // BS3 only @test
+define('GA_MD12', 'col-md-12'); // BS3 only @test
+define('GA_LG1', 'col-lg-1'); // BS3 only @test
+define('GA_LG2', 'col-lg-2'); // BS3 only @test
+define('GA_LG3', 'col-lg-3'); // BS3 only @test
+define('GA_LG4', 'col-lg-4'); // BS3 only @test
+define('GA_LG5', 'col-lg-5'); // BS3 only @test
+define('GA_LG6', 'col-lg-6'); // BS3 only @test
+define('GA_LG7', 'col-lg-7'); // BS3 only @test
+define('GA_LG8', 'col-lg-8'); // BS3 only @test
+define('GA_LG9', 'col-lg-9'); // BS3 only @test
+define('GA_LG10', 'col-lg-10'); // BS3 only @test
+define('GA_LG11', 'col-lg-11'); // BS3 only @test
+define('GA_LG12', 'col-lg-12'); // BS3 only @test
+define('GA_XS_OF1', 'col-xs-offset-1'); // BS3 only @test
+define('GA_XS_OF2', 'col-xs-offset-2'); // BS3 only @test
+define('GA_XS_OF3', 'col-xs-offset-3'); // BS3 only @test
+define('GA_XS_OF4', 'col-xs-offset-4'); // BS3 only @test
+define('GA_XS_OF5', 'col-xs-offset-5'); // BS3 only @test
+define('GA_XS_OF6', 'col-xs-offset-6'); // BS3 only @test
+define('GA_XS_OF7', 'col-xs-offset-7'); // BS3 only @test
+define('GA_XS_OF8', 'col-xs-offset-8'); // BS3 only @test
+define('GA_XS_OF9', 'col-xs-offset-9'); // BS3 only @test
+define('GA_XS_OF10', 'col-xs-offset-10'); // BS3 only @test
+define('GA_XS_OF11', 'col-xs-offset-11'); // BS3 only @test
+define('GA_XS_OF12', 'col-xs-offset-12'); // BS3 only @test
+define('GA_SM_OF1', 'col-sm-offset-1'); // BS3 only @test
+define('GA_SM_OF2', 'col-sm-offset-2'); // BS3 only @test
+define('GA_SM_OF3', 'col-sm-offset-3'); // BS3 only @test
+define('GA_SM_OF4', 'col-sm-offset-4'); // BS3 only @test
+define('GA_SM_OF5', 'col-sm-offset-5'); // BS3 only @test
+define('GA_SM_OF6', 'col-sm-offset-6'); // BS3 only @test
+define('GA_SM_OF7', 'col-sm-offset-7'); // BS3 only @test
+define('GA_SM_OF8', 'col-sm-offset-8'); // BS3 only @test
+define('GA_SM_OF9', 'col-sm-offset-9'); // BS3 only @test
+define('GA_SM_OF10', 'col-sm-offset-10'); // BS3 only @test
+define('GA_SM_OF11', 'col-sm-offset-11'); // BS3 only @test
+define('GA_SM_OF12', 'col-sm-offset-12'); // BS3 only @test
+define('GA_MD_OF1', 'col-md-offset-1'); // BS3 only @test
+define('GA_MD_OF2', 'col-md-offset-2'); // BS3 only @test
+define('GA_MD_OF3', 'col-md-offset-3'); // BS3 only @test
+define('GA_MD_OF4', 'col-md-offset-4'); // BS3 only @test
+define('GA_MD_OF5', 'col-md-offset-5'); // BS3 only @test
+define('GA_MD_OF6', 'col-md-offset-6'); // BS3 only @test
+define('GA_MD_OF7', 'col-md-offset-7'); // BS3 only @test
+define('GA_MD_OF8', 'col-md-offset-8'); // BS3 only @test
+define('GA_MD_OF9', 'col-md-offset-9'); // BS3 only @test
+define('GA_MD_OF10', 'col-md-offset-10'); // BS3 only @test
+define('GA_MD_OF11', 'col-md-offset-11'); // BS3 only @test
+define('GA_MD_OF12', 'col-md-offset-12'); // BS3 only @test
+define('GA_LG_OF1', 'col-lg-offset-1'); // BS3 only @test
+define('GA_LG_OF2', 'col-lg-offset-2'); // BS3 only @test
+define('GA_LG_OF3', 'col-lg-offset-3'); // BS3 only @test
+define('GA_LG_OF4', 'col-lg-offset-4'); // BS3 only @test
+define('GA_LG_OF5', 'col-lg-offset-5'); // BS3 only @test
+define('GA_LG_OF6', 'col-lg-offset-6'); // BS3 only @test
+define('GA_LG_OF7', 'col-lg-offset-7'); // BS3 only @test
+define('GA_LG_OF8', 'col-lg-offset-8'); // BS3 only @test
+define('GA_LG_OF9', 'col-lg-offset-9'); // BS3 only @test
+define('GA_LG_OF10', 'col-lg-offset-10'); // BS3 only @test
+define('GA_LG_OF11', 'col-lg-offset-11'); // BS3 only @test
+define('GA_LG_OF12', 'col-lg-offset-12'); // BS3 only @test
+
+// Displays
+define('GA_INLINE_BLOCK', 'inline-block'); // BS3 only @test
+
+// States
+define('GA_DEFAULT', 'default'); // Bs3
+
+// Button
+define('GA_BTN_DEFAULT', 'btn-default'); // BS3
+
+// NAVBAR
+define('GA_BTN_NAVBAR', 'btn-navbar'); // Bs3
+
+// BADGES
+define('GA_BADGE_DEFAULT', 'badge-default'); // Bs3
+
+// RESPONSIVE UTILITIES
+define('GA_VISIBLE_XS_BLOCK', 'visible-xs-block'); // BS3 only @test
+define('GA_VISIBLE_XS_INLINE', 'visible-xs-inline'); // BS3 only @test
+define('GA_VISIBLE_XS_INLINE_BLOCK', 'visible-xs-inline-block'); // BS3 only @test
+define('GA_VISIBLE_SM_BLOCK', 'visible-sm-block'); // BS3 only @test
+define('GA_VISIBLE_SM_INLINE', 'visible-sm-inline'); // BS3 only @test
+define('GA_VISIBLE_SM_INLINE_BLOCK', 'visible-sm-inline-block'); // BS3 only @test
+define('GA_VISIBLE_MD_BLOCK', 'visible-md-block'); // BS3 only @test
+define('GA_VISIBLE_MD_INLINE', 'visible-md-inline'); // BS3 only @test
+define('GA_VISIBLE_MD_INLINE_BLOCK', 'visible-md-inline-block'); // BS3 only @test
+define('GA_VISIBLE_LG_BLOCK', 'visible-lg-block'); // BS3 only @test
+define('GA_VISIBLE_LG_INLINE', 'visible-lg-inline'); // BS3 only @test
+define('GA_VISIBLE_LG_INLINE_BLOCK', 'visible-lg-inline-block'); // BS3 only @test
+define('GA_HIDDEN_XS', 'hidden-xs'); // BS3 only @test
+define('GA_HIDDEN_SM', 'hidden-sm'); // BS3 only @test
+define('GA_HIDDEN_MD', 'hidden-md'); // BS3 only @test
+define('GA_HIDDEN_LG', 'hidden-lg'); // BS3 only @test
+define('GA_VISIBLE_PRINT_BLOCK', 'visible-print-block'); // BS3 only @test
+define('GA_VISIBLE_PRINT_INLINE', 'visible-print-inline'); // BS3 only @test
+define('GA_VISIBLE_PRINT_INLINE_BLOCK', 'visible-print-inline-block'); // BS3 only @test
+define('GA_HIDDEN_PRINT', 'hidden-print'); // BS3 only @test
+
